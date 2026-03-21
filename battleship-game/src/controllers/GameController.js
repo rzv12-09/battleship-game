40889da6c40de62ff8e8b6319d1de9163a98f419
rzv12-09 <olariurazvan20@gmail.com>
2026-03-21 22:03:48 +0200

    feat: implement playRound logic

--- battleship-game/src/controllers/GameController.js
+++ battleship-game/src/controllers/GameController.js
@@ -4,4 +4,22 @@ export default class GameController {
     this.player2 = player2;
     this.activePlayer = player1;
   }
+
+  playRound(row, col) {
+    const enemyPlayer =
+      this.activePlayer === this.player1 ? this.player2 : this.player1;
+    const enemyBoard = enemyPlayer.gameBoard;
+    enemyBoard.receiveAttack(row, col);
+    this.activePlayer = enemyPlayer;
+  }
+
+  checkWinnerPlayer() {
+    if (this.player1.gameBoard.areAllShipsSunk()) {
+      return this.player2.name;
+    }
+    if (this.player2.gameBoard.areAllShipsSunk()) {
+      return this.player1.name;
+    }
+    return null;
+  }
 }
